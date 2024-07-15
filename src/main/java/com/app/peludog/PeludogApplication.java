1360695eff3179d249d0a9fea14c04d6dc081dbd
unknown <educar200@gmail.com>
2024-07-15 18:56:57 +0200

    first commit

---
+++ src/main/java/com/app/peludog/PeludogApplication.java
@@ -0,0 +1,13 @@
+package com.app.peludog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeludogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeludogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/peludog/PeludogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.peludog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeludogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
